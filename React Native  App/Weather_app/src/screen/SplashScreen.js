5ec1024094ddc6175406af2dbe5f1f9c3c5ae459
Nitish Biswas <55139122+NITISHBISWAS@users.noreply.github.com>
2021-04-05 23:37:53 +0600

    app.js change

---
+++ React Native  App/Weather_app/src/screen/SplashScreen.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {View, Image, StyleSheet, StatusBar, TouchableOpacity, Text} from 'react-native';
+
+const SplashScreen = () => {
+  return(
+    <View style={styles.imageView}>
+      <StatusBar animated={true} backgroundColor='#57aee2' />
+      <Image source={require('../image/img.png')} style={styles.imageView} />
+      <TouchableOpacity style={styles.textView}>
+        <Text style={styles.text}>Get Started ></Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  imageView: {
+    flex: 1,
+    height: '100%',
+    width: '100%',
+  },
+  textView: {
+    position: 'absolute',
+    bottom: '8%',
+    right: '8%',
+    borderWidth: 2,
+    borderColor: '#57aee2',
+    borderRadius: 7,
+    padding: 10,
+  },
+  text: {
+    fontSize: 20,
+    color: 'white',
+  }
+});
+
+export default SplashScreen;
