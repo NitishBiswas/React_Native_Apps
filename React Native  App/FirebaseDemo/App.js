51b7a4be0e1368c7e6b4f3a1ed288da5de24db08
Nitish Biswas <55139122+NITISHBISWAS@users.noreply.github.com>
2021-05-07 15:20:42 +0600

    modify code

--- React Native  App/FirebaseDemo/App.js
+++ React Native  App/FirebaseDemo/App.js
@@ -7,10 +7,12 @@ import {
   View,
   TextInput,
   TouchableOpacity,
-  Alert,
   LogBox,
+  StatusBar,
+  Image,
 } from 'react-native';
 import firebase from 'firebase';
+import LinearGradient from 'react-native-linear-gradient';
 import { config } from './config';
 
 const App = () => {
@@ -24,65 +26,97 @@ const App = () => {
     firebase.app(); // if already initialized, use that one
   }
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <TextInput
-        placeholder="Enter Name"
-        value={name}
-        style={{ width: '80%', padding: 7, fontSize: 20, borderBottomWidth: 1, marginBottom: 10 }}
-        onChangeText={(text) => setName(text)}
-      />
-      <TextInput
-        placeholder="Enter Roll"
-        value={roll}
-        style={{ width: '80%', padding: 7, fontSize: 20, borderBottomWidth: 1, marginBottom: 10 }}
-        onChangeText={(text) => setRoll(text)}
-      />
-      <TouchableOpacity
-        onPress={() => {
-          // var users = firebase.database().ref('user');
-          // console.log(users);
-          // users.once('value', (snapshot) => {
-          //   setData(snapshot.toJSON());
-          //   console.log(data);
-          // });
-          var userRef = firebase.database().ref(`user/${name}`);
-          var exists;
-          userRef.once('value', (snapshot) => {
-            exists = snapshot.exists();
-            setData(snapshot.toJSON());
-          })
-            .then(() => {
-              if (exists) {
-                if (data.Name.includes('N')) {
-                  console.log('Yes');
-                }
-                if (data.Name === name && data.Roll === roll) {
-                  alert('Maching');
-                }
-              } else {
-                userRef.set({
-                  Name: name,
-                  Roll: roll,
-                })
-                  .then(() => Alert.alert('Inserted!'))
-                  .catch(() => console.log('Error'));
-              }
-            })
-        }}
-        style={{ width: '80%' }}>
-        <Text
+    <LinearGradient style={{ flex: 1, justifyContent: 'space-between', backgroundColor: 'white' }} colors={['#FF4700', '#fff']} start={{ x: 0, y: 0 }} end={{ x: 0, y: 1 }}>
+      <StatusBar backgroundColor="#FF4700" />
+      <View style={{ alignItems: 'center', marginTop: 20 }}>
+        <View style={{ borderWidth: 2, borderRadius: 100, borderColor: 'white', elevation: 20 }}>
+          <Image source={require('./images/logo.jpg')} style={{ height: 200, width: 200, borderRadius: 100 }} />
+        </View>
+      </View>
+      <View style={{ alignItems: 'center', backgroundColor: 'white', height: '60%', borderTopStartRadius: 50, borderTopEndRadius: 50, elevation: 4 }}>
+        <TextInput
+          placeholder="Enter Name"
+          value={name}
           style={{
-            fontSize: 22,
-            borderWidth: 2,
-            padding: 5,
-            width: '100%',
-            textAlign: 'center',
-            borderColor: 'purple',
-            borderRadius: 5,
-            marginTop: 10,
-          }}>Save</Text>
-      </TouchableOpacity>
-    </View>
+            width: '80%',
+            padding: 10,
+            fontSize: 20,
+            // borderWidth: 2,
+            marginVertical: 20,
+            // borderColor: '#ff2e63',
+            borderRadius: 20,
+            color: '#FF4700',
+            elevation: 4,
+            backgroundColor: 'white',
+          }}
+          onChangeText={(text) => setName(text)}
+        />
+        <TextInput
+          placeholder="Enter Roll"
+          value={roll}
+          style={{
+            width: '80%',
+            padding: 10,
+            fontSize: 20,
+            // borderWidth: 2,
+            marginBottom: 10,
+            // borderColor: '#ff2e63',
+            borderRadius: 20,
+            color: '#FF4700',
+            elevation: 4,
+            backgroundColor: 'white',
+          }}
+          onChangeText={(text) => setRoll(text)}
+        />
+        <TouchableOpacity
+          onPress={() => {
+            // var users = firebase.database().ref('user');
+            // console.log(users);
+            // users.once('value', (snapshot) => {
+            //   setData(snapshot.toJSON());
+            //   console.log(data);
+            // });
+            // var userRef = firebase.database().ref(`user/${name}`);
+            // var exists;
+            // userRef.once('value', (snapshot) => {
+            //   exists = snapshot.exists();
+            //   setData(snapshot.toJSON());
+            // })
+            //   .then(() => {
+            //     if (exists) {
+            //       if (data.Name.includes('N')) {
+            //         console.log('Yes');
+            //       }
+            //       if (data.Name === name && data.Roll === roll) {
+            //         alert('Maching');
+            //       }
+            //     } else {
+            //       userRef.set({
+            //         Name: name,
+            //         Roll: roll,
+            //       })
+            //         .then(() => Alert.alert('Inserted!'))
+            //         .catch(() => console.log('Error'));
+            //     }
+            //   })
+          }}
+          style={{ width: '80%', elevation: 4, backgroundColor: 'white', borderRadius: 20, marginTop: 15 }}>
+          <Text
+            style={{
+              fontSize: 22,
+              // borderWidth: 2,
+              padding: 8,
+              width: '100%',
+              textAlign: 'center',
+              // borderColor: '#ff2e63',
+              borderRadius: 10,
+              // marginTop: 10,
+              color: '#FF4700',
+            }}>Save</Text>
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
+
   );
 };
 
